7360a5e6b63a141eee5ac9d6f4797a17eb315063
limoiie <limo.iie4@gmail.com>
2021-01-01 01:38:03 +0800

    feat: Add elpy

--- init.el
+++ init.el
@@ -426,4 +426,10 @@ If region is active, adds or removes vimish folds."
    )
   )
 
+(use-package elpy
+  :ensure t
+  :defer t
+  :init
+  (advice-add 'python-mode :before 'elpy-enable))
+
 ;;; init.el ends here

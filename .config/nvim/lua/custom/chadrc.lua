5ccfd096e6b00d27e659e78f6cf5cffc1fd6a794
limoiie <limo.iie4@gmail.com>
2021-11-05 12:39:33 +0000

    refactor: Add lspconfig into .config/nvim

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -26,6 +26,9 @@ M.plugins = {
       --    path of file containing setups of different lsps (ex : "custom.plugins.lspconfig"), read the docs for more info
       --    setup_lspconf = "",
       --   },
+      lspconfig = {
+        setup_lspconf = 'custom.plugins.lspconfig'
+      }
    },
    -- To change the Packer `config` of a plugin that comes with NvChad,
    -- add a table entry below matching the plugin github name
--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -3,6 +3,7 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
 Plug('haya14busa/vim-easymotion')
+Plug('neovim/nvim-lspconfig')
 Plug('Pocco81/TrueZen.nvim')
 Plug('tpope/vim-surround')
 

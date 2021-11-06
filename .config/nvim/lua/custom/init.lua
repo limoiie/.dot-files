192b8c9a0dab16702c7454bc205f40c259c2ac40
limoiie <limo.iie4@gmail.com>
2021-11-06 11:08:07 +0800

    refactor: Add which-key plugin for nvim

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -2,6 +2,7 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
+Plug('folke/which-key.nvim')
 Plug('haya14busa/vim-easymotion')
 Plug('neovim/nvim-lspconfig')
 Plug('Pocco81/TrueZen.nvim')

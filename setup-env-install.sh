21d69ff5e4d855a28ef161dd757bd477b4820245
limoiie <limo.iie4@gmail.com>
2022-11-28 21:04:10 +0800

    refactor: Merge the two install scripts

--- setup-env-install.sh
+++ setup-env-install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+install-useful-dist-tools() {
+    set -e
+
+    echo "Install dist packages..."
+    apt-get update -o Acquire::http::proxy=false \
+        && apt-get install -o Acquire::http::proxy=false -y \
+                   build-essential \
+                   curl \
+                   gawk \
+                   git \
+                   zsh \
+        || exit -1
+    apt-get install -o Acquire::http::proxy=false -y software-properties-common \
+        && add-apt-repository -y ppa:neovim-ppa/stable \
+        && add-apt-repository -y ppa:ubuntu-elisp/ppa \
+        && apt-get update -o Acquire::http::proxy=false \
+        && apt-get install -o Acquire::http::proxy=false -y \
+                   neovim \
+                   emacs-snapshot \
+        || exit -1
+
+    set +e
+}
+
 install-useful-modern-tools() {
     set -e
 
@@ -38,5 +62,5 @@ run-remote-script() {
     set +eu
 }
 
+install-useful-dist-tools
 install-useful-modern-tools
-
--- setup-env.sh
+++ setup-env.sh
@@ -3,6 +3,5 @@
 rawcontent=https://raw.githubusercontent.com
 action=${1:-update}
 
-curl -L $rawcontent/limoiie/.dot-files/master/setup-env-install-dist-tools.sh | bash -s && \
-curl -L $rawcontent/limoiie/.dot-files/master/setup-env-install-modern-tools.sh | bash -s && \
+curl -L $rawcontent/limoiie/.dot-files/master/setup-env-install.sh | bash -s && \
 curl -L $rawcontent/limoiie/.dot-files/master/setup-env-config.sh | bash -s -- $action

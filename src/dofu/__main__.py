c77c098e42bf72168ddfcc6728a630ac549a5515
limoiie <limo.iie4@gmail.com>
2024-01-28 23:14:08 +0800

    [refactor] Add list and install commands to cli

--- src/dofu/__main__.py
+++ src/dofu/__main__.py
@@ -44,6 +44,49 @@ class App:
 
     @staticmethod
     @extend_interface(init)
+    def list(*module_names: str, installed_only: bool = None):
+        """
+        List modules.
+
+        List all the modules, or the modules with the given names.
+
+        :param module_names: The names of modules to list.
+        :param installed_only: Whether to list installed modules only.
+        """
+        # load module equipment meta information
+        manager = ModuleEquipmentManager.load()
+
+        installed_module_names = set(manager.equipped_module_names())
+        module_names = module_names or ModuleRegistrationManager.all_module_names()
+        blueprint = ModuleRegistrationManager.resolve_equip_blueprint(module_names)
+
+        for module in blueprint:
+            is_installed = module.name() in installed_module_names
+            if installed_only and not is_installed:
+                continue
+
+            _logger.info(
+                f"Module {module.name()} " +
+                ("[green]Installed[/]" if is_installed else "[red]Not Installed[/red]"),
+            )
+            _logger.debug("- Requirements: ")
+
+            _logger.debug(f"  - Packages:")
+            for pkg in module.package_requirements():
+                _logger.debug(f"      {pkg.spec.package}@{pkg.spec.version}")
+
+            _logger.debug(f"  - GitRepos")
+            for git in module.gitrepo_requirements():
+                _logger.debug(f"      {git.url}")
+
+            _logger.debug(f"  - Commands")
+            for cmd in module.command_requirements():
+                _logger.debug(f"      {cmd.cmdline()[:40]}")
+
+            _logger.debug(f"--")
+
+    @staticmethod
+    @extend_interface(init)
     def equip(*module_names: str):
         """
         Equip modules.
@@ -64,8 +107,48 @@ class App:
                 *ModuleRegistrationManager.all_module_names(),
                 header="Choose modules to equip",
                 no_limit=True,
-                select_if_one=True,
                 selected=manager.equipped_module_names(),
+            )
+            .strip()
+            .split("\n")
+            if not module_names
+            else module_names
+        )
+
+        module_names = list(filter(None, module_names))
+        if module_names:
+            manager.equip(module_names)
+
+    @staticmethod
+    @extend_interface(init)
+    def install(*module_names: str):
+        """
+        Install modules.
+
+        Similar with equip, but can only choose uninstalled modules.
+
+        Equip the modules with the given names.
+        If no modules are given, you will be asked to choose modules to equip.
+        All the dependencies of the modules will be equipped as well.
+        The order of equipping is determined by the dependency graph automatically.
+
+        :param module_names: The names of modules to equip.
+        """
+        # load module equipment meta information
+        manager = ModuleEquipmentManager.load()
+
+        equipped = set(manager.equipped_module_names())
+
+        # choose modules to equip
+        module_names = (
+            gum.choose(
+                *(
+                    module
+                    for module in ModuleRegistrationManager.all_module_names()
+                    if module not in equipped
+                ),
+                header="Choose modules to install",
+                no_limit=True,
             )
             .strip()
             .split("\n")
@@ -104,7 +187,6 @@ class App:
                 *equipped_module_names,
                 header="Choose modules to remove",
                 no_limit=True,
-                select_if_one=True,
             )
             .strip()
             .split("\n")

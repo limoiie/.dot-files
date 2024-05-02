cfc50bb2edf6d0f80f759486d88ee31a61a37888
limoiie <limo.iie4@gmail.com>
2024-05-02 21:34:55 +0800

    [lspconfig] Add hls for Haskell

--- xdg-config/NvChad/lua/custom/configs/lspconfig.lua
+++ xdg-config/NvChad/lua/custom/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "pyright", "rust_analyzer", "ocamllsp" }
+local servers = { "pyright", "rust_analyzer", "ocamllsp", "hls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {

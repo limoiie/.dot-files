b47777e7239b227a5b7920933e4423b751e88ee5
limoiie <limo.iie4@gmail.com>
2024-05-02 23:29:43 +0800

    [ui] Adjust statusline theme

--- xdg-config/NvChad/lua/custom/chadrc.lua
+++ xdg-config/NvChad/lua/custom/chadrc.lua
@@ -3,8 +3,13 @@
 local M = {}
 
 M.ui = {
+  transparency = true,
   theme_toggle = { "aquarium", "one_light" },
   theme = "aquarium",
+  statusline = {
+    theme = "vscode_colored", -- default,vscode,vscode_colored,minimal
+    separator_style = "default" -- default: round,block,arrow; minimal: round
+  },
   nvdash = {
     load_on_startup = true,
   }
